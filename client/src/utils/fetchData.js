5fe47ab15d671aca6e76d9bfe08b8303a03670d9
lamminhhuy <lhuy00429@gmail.com>
2023-05-26 10:29:30 +0700

    update client

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { URL } from '../../Url';
+import { URL } from './Url';
 
 export const getDataAPI = async (url, token) => {
     const res = await axios.get(`${URL}/api/${url}`, {

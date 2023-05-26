2c06603e10f1d59afa5c7b9f9f86cb866917076c
lamminhhuy <lhuy00429@gmail.com>
2023-05-26 13:45:28 +0700

    update client

--- client/src/utils/Url.js
+++ client/src/utils/Url.js
@@ -1,1 +1,3 @@
-export const URL = process.env.REACT_APP_SERVER_URL;
+
+export const URL = process.env.NODE_ENV === 'production' ? 
+'https://readchoiceapis2.onrender.com/' : 'http://localhost:5000'

10649c4466e46ce0ab51f4cdba804a0b767a2082
lamminhhuy <lhuy00429@gmail.com>
2023-05-28 23:02:07 +0700

    update server

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const { ExpressPeerServer } = require('peer')
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin:'https://readchoice.vercel.app'
+    origin:'*'
   }));
 app.use(cookieParser())
 app.get('/',(req,res)=> {
